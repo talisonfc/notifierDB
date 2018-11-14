9c08c815051a3a30cff6300c702a4d3c85a67cc3
Talison <tfccomputation@gmail.com>
2018-11-14 15:55:55 -0300

    update

--- scripts.sql
+++ scripts.sql
@@ -1,5 +1,5 @@
 /*
-create function sendtest5(a integer)
+create function sendtest8()
 returns trigger
 as $$
 import requests
@@ -8,21 +8,93 @@ import json
 url = "http://localhost:8080/send"
 headers = {'Content-Type': 'application/json'}
 
-payload = {'application': 'pje', 'id': a}
+payload = {'application': 'pje', 'data': TD['new']}
 r = requests.post(url, data=json.dumps(payload), headers=headers)
 $$ language plpythonu;
-
-*/
-
-
-/*
-select sendtest3(id) from usuario;
 */
 
 /*
-CREATE TRIGGER tgtest BEFORE INSERT OR UPDATE OR DELETE
+create function sendtest9()
+returns trigger
+as $$
+import requests
+import json
+
+url = "http://localhost:8080/send"
+headers = {'Content-Type': 'application/json'}
+
+payload = {'application': 'pje', 'id': TD['new']['id']}
+r = requests.post(url, data=json.dumps(payload), headers=headers)
+$$ language plpythonu;
+*/
+
+/*
+select sendtest6(id) from usuario;
+*/
+
+/*
+CREATE TRIGGER tgtest3 AFTER INSERT
    ON usuario FOR EACH ROW
-   EXECUTE PROCEDURE sendtest5();
+   EXECUTE PROCEDURE sendtest8();
 */
 
-insert into usuario values (2,'maria','322.342.234-33','maria@gmail.com','123456')
+insert into usuario values (21,'Antonio','322.342.234-33','maria@gmail.com','123456')
+
+/*
+criando trigger passando argumento
+*/
+/*
+create function sendtest10()
+returns trigger
+as $$
+import requests
+import json
+
+url = "http://localhost:8080/send"
+headers = {'Content-Type': 'application/json'}
+
+payload = {'application': TD['args'][0], 'id': TD['new']['id']}
+r = requests.post(url, data=json.dumps(payload), headers=headers)
+$$ language plpythonu;
+*/
+
+/*
+select sendtest6(id) from usuario;
+*/
+
+/*
+CREATE TRIGGER tgtest AFTER INSERT
+   ON usuario FOR EACH ROW
+   EXECUTE PROCEDURE sendtest10 ('pje');
+*/
+
+/*
+>> Usando padrão
+
+create function sendtest12()
+returns trigger
+as $$
+import requests
+import json
+
+url = "http://localhost:8080/send"
+headers = {'Content-Type': 'application/json'}
+
+payload = {'source': TD['args'][0], 'table': TD['args'][1], 'data': TD['new']}
+r = requests.post(url, data=json.dumps(payload), headers=headers)
+$$ language plpythonu;
+*/
+
+/*
+select sendtest6(id) from usuario;
+*/
+
+/*
+CREATE TRIGGER tgtest AFTER INSERT
+   ON usuario FOR EACH ROW
+   EXECUTE PROCEDURE sendtest12 ('pje','usuario');
+*/
+
+/*
+insert into usuario values (21,'wellison','322.342.234-33','wellison@gmail.com','123456')
+*/
